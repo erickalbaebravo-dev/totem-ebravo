60f913c5be8b506f2b83166eb0f00824ecc12ddc
erickalbaebravo-dev <erickalbaebravo@gmail.com>
2025-12-03 10:44:27 -0300

    Update gravar.js

--- api/gravar.js
+++ api/gravar.js
@@ -19,8 +19,9 @@ export default async function handler(req, res) {
     }
 
     await pool.query(
-      "INSERT INTO votos (nota, origem) VALUES ($1, $2)"    );
-    await pool.query(q, [nota, origem]);
+      "INSERT INTO votos (nota, origem) VALUES ($1, $2)",
+      [nota, origem]
+    );
     
     return res.status(200).json({ ok: true });
 
